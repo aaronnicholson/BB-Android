2ebdc890e748dd715e1b347fb8471c04af05e914
Aaron Nicholson <aaron@thecodebuilders.com>
2015-09-17 22:08:35 -0400

    in the middle of getting video downloads working

--- BabysBrilliant/app/src/main/java/com/thecodebuilders/babysbrilliant/MainActivity.java
+++ BabysBrilliant/app/src/main/java/com/thecodebuilders/babysbrilliant/MainActivity.java
@@ -1,5 +1,6 @@
 package com.thecodebuilders.babysbrilliant;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -7,6 +8,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -33,6 +35,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements PlaylistChooser.PlaylistChooserListener{
@@ -99,12 +102,28 @@ public class MainActivity extends AppCompatActivity implements PlaylistChooser.P
     static Runnable delayedHide = null;
     static Boolean doHideControls = false;
 
+    // Used to communicate state changes in the DownloaderThread
+    public static final int MESSAGE_DOWNLOAD_STARTED = 1000;
+    public static final int MESSAGE_DOWNLOAD_COMPLETE = 1001;
+    public static final int MESSAGE_UPDATE_PROGRESS_BAR = 1002;
+    public static final int MESSAGE_DOWNLOAD_CANCELED = 1003;
+    public static final int MESSAGE_CONNECTING_STARTED = 1004;
+    public static final int MESSAGE_ENCOUNTERED_ERROR = 1005;
+
+    // Downloader instance variables
+    private static MainActivity thisActivity;
+    private static Thread downloaderThread;
+    private static ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloaderThread = null;
+        progressDialog = null;
 
         assetsURL = getString(R.string.assets_url);
 
@@ -498,9 +517,13 @@ public class MainActivity extends AppCompatActivity implements PlaylistChooser.P
 
     }
 
-    public static void playVideo(String videoURL) {
+    public void playVideo(String videoURL) {
         String url = mediaURL + videoURL;
+        //TODO: temporary for testing
+        downloadItem(url);
 
+
+        /*
         videoToggleButton.setText(appContext.getString(R.string.video_pause));
         videoLayout.setVisibility(View.VISIBLE);
         videoView.setVideoPath(url);
@@ -510,7 +533,7 @@ public class MainActivity extends AppCompatActivity implements PlaylistChooser.P
         videoView.setAlpha(0); //hide prior to media being ready
         //TODO: show preloader
 
-        showControls();
+        showControls();*/
     }
 
     private static void showControls() {
@@ -601,7 +624,6 @@ public class MainActivity extends AppCompatActivity implements PlaylistChooser.P
         }
     }
 
-
     private void initApp() throws JSONException {
         //TODO: add preloader
         configureThumbnailList(jsonData.getJSONArray(currentMenu));
@@ -630,5 +652,180 @@ public class MainActivity extends AppCompatActivity implements PlaylistChooser.P
         return super.onOptionsItemSelected(item);
     }
 
+    //DOWNLOAD
+    void downloadItem(String downloadURL) {
+        downloaderThread = new DownloaderThread(thisActivity, downloadURL);
+        downloaderThread.start();
+    }
 
+    /**
+     * This is the Handler for this activity. It will receive messages from the
+     * DownloaderThread and make the necessary updates to the UI.
+     */
+//    public Handler activityHandler = new Handler()
+//    {
+//        public void handleMessage(Message msg)
+//        {
+//            switch(msg.what)
+//            {
+//				/*
+//				 * Handling MESSAGE_UPDATE_PROGRESS_BAR:
+//				 * 1. Get the current progress, as indicated in the arg1 field
+//				 *    of the Message.
+//				 * 2. Update the progress bar.
+//				 */
+//                case MESSAGE_UPDATE_PROGRESS_BAR:
+//                    if(progressDialog != null)
+//                    {
+//                        int currentProgress = msg.arg1;
+//                        progressDialog.setProgress(currentProgress);
+//                    }
+//                    break;
+//
+//				/*
+//				 * Handling MESSAGE_CONNECTING_STARTED:
+//				 * 1. Get the URL of the file being downloaded. This is stored
+//				 *    in the obj field of the Message.
+//				 * 2. Create an indeterminate progress bar.
+//				 * 3. Set the message that should be sent if user cancels.
+//				 * 4. Show the progress bar.
+//				 */
+//                case MESSAGE_CONNECTING_STARTED:
+//                    if(msg.obj != null && msg.obj instanceof String)
+//                    {
+//                        String url = (String) msg.obj;
+//                        // truncate the url
+//                        if(url.length() > 16)
+//                        {
+//                            String tUrl = url.substring(0, 15);
+//                            tUrl += "...";
+//                            url = tUrl;
+//                        }
+//                        String pdTitle = thisActivity.getString(R.string.progress_dialog_title_connecting);
+//                        String pdMsg = thisActivity.getString(R.string.progress_dialog_message_prefix_connecting);
+//                        pdMsg += " " + url;
+//
+//                        dismissCurrentProgressDialog();
+//                        progressDialog = new ProgressDialog(thisActivity);
+//                        progressDialog.setTitle(pdTitle);
+//                        progressDialog.setMessage(pdMsg);
+//                        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+//                        progressDialog.setIndeterminate(true);
+//                        // set the message to be sent when this dialog is canceled
+//                        Message newMsg = Message.obtain(this, MESSAGE_DOWNLOAD_CANCELED);
+//                        progressDialog.setCancelMessage(newMsg);
+//                        progressDialog.show();
+//                    }
+//                    break;
+//
+//				/*
+//				 * Handling MESSAGE_DOWNLOAD_STARTED:
+//				 * 1. Create a progress bar with specified max value and current
+//				 *    value 0; assign it to progressDialog. The arg1 field will
+//				 *    contain the max value.
+//				 * 2. Set the title and text for the progress bar. The obj
+//				 *    field of the Message will contain a String that
+//				 *    represents the name of the file being downloaded.
+//				 * 3. Set the message that should be sent if dialog is canceled.
+//				 * 4. Make the progress bar visible.
+//				 */
+//                case MESSAGE_DOWNLOAD_STARTED:
+//                    // obj will contain a String representing the file name
+//                    if(msg.obj != null && msg.obj instanceof String)
+//                    {
+//                        int maxValue = msg.arg1;
+//                        String fileName = (String) msg.obj;
+//                        String pdTitle = thisActivity.getString(R.string.progress_dialog_title_downloading);
+//                        String pdMsg = thisActivity.getString(R.string.progress_dialog_message_prefix_downloading);
+//                        pdMsg += " " + fileName;
+//
+//                        dismissCurrentProgressDialog();
+//                        progressDialog = new ProgressDialog(thisActivity);
+//                        progressDialog.setTitle(pdTitle);
+//                        progressDialog.setMessage(pdMsg);
+//                        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+//                        progressDialog.setProgress(0);
+//                        progressDialog.setMax(maxValue);
+//                        // set the message to be sent when this dialog is canceled
+//                        Message newMsg = Message.obtain(this, MESSAGE_DOWNLOAD_CANCELED);
+//                        progressDialog.setCancelMessage(newMsg);
+//                        progressDialog.setCancelable(true);
+//                        progressDialog.show();
+//                    }
+//                    break;
+//
+//				/*
+//				 * Handling MESSAGE_DOWNLOAD_COMPLETE:
+//				 * 1. Remove the progress bar from the screen.
+//				 * 2. Display Toast that says download is complete.
+//				 */
+//                case MESSAGE_DOWNLOAD_COMPLETE:
+//                    dismissCurrentProgressDialog();
+//                    displayMessage(getString(R.string.user_message_download_complete));
+//                    break;
+//
+//				/*
+//				 * Handling MESSAGE_DOWNLOAD_CANCELLED:
+//				 * 1. Interrupt the downloader thread.
+//				 * 2. Remove the progress bar from the screen.
+//				 * 3. Display Toast that says download is complete.
+//				 */
+//                case MESSAGE_DOWNLOAD_CANCELED:
+//                    if(downloaderThread != null)
+//                    {
+//                        downloaderThread.interrupt();
+//                    }
+//                    dismissCurrentProgressDialog();
+//                    displayMessage(getString(R.string.user_message_download_canceled));
+//                    break;
+//
+//				/*
+//				 * Handling MESSAGE_ENCOUNTERED_ERROR:
+//				 * 1. Check the obj field of the message for the actual error
+//				 *    message that will be displayed to the user.
+//				 * 2. Remove any progress bars from the screen.
+//				 * 3. Display a Toast with the error message.
+//				 */
+//                case MESSAGE_ENCOUNTERED_ERROR:
+//                    // obj will contain a string representing the error message
+//                    if(msg.obj != null && msg.obj instanceof String)
+//                    {
+//                        String errorMessage = (String) msg.obj;
+//                        dismissCurrentProgressDialog();
+//                        displayMessage(errorMessage);
+//                    }
+//                    break;
+//
+//                default:
+//                    // nothing to do here
+//                    break;
+//            }
+//        }
+//    };
+//
+//    /**
+//     * If there is a progress dialog, dismiss it and set progressDialog to
+//     * null.
+//     */
+//    public void dismissCurrentProgressDialog()
+//    {
+//        if(progressDialog != null)
+//        {
+//            progressDialog.hide();
+//            progressDialog.dismiss();
+//            progressDialog = null;
+//        }
+//    }
+//
+//    /**
+//     * Displays a message to the user, in the form of a Toast.
+//     * @param message Message to be displayed.
+//     */
+//    public void displayMessage(String message)
+//    {
+//        if(message != null)
+//        {
+//            Toast.makeText(thisActivity, message, Toast.LENGTH_SHORT).show();
+//        }
+//    }
 }
